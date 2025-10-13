e60b0470ddaec25b708d34f86b4a2fade1ce76ff
aliNassef <anassef798@gmail.com>
2025-10-13 13:30:14 +0300

    refactor profile feature

--- lib/features/profile/presentation/widgets/profile_view_body.dart
+++ lib/features/profile/presentation/widgets/profile_view_body.dart
@@ -4,18 +4,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:hungry/core/extensions/mediaquery_size.dart';
-import 'package:hungry/core/utils/app_dilagos.dart';
 import 'package:hungry/core/widgets/custom_failure_widget.dart';
-import 'package:hungry/features/auth/presentation/views/login_view.dart';
 import 'package:hungry/features/profile/presentation/controller/profile_cubit/profile_cubit.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
-import '../../../../core/navigation/app_navigation.dart';
 import '../../../../core/utils/app_assets.dart';
 import '../../../../core/utils/app_colors.dart';
 import '../../../../core/utils/app_shadwo.dart';
 import '../../../../core/widgets/custom_text_form_field.dart';
 import '../../../../core/widgets/default_app_button.dart';
+import 'logout_button_bloc_listener.dart';
 import 'profile_top_bar.dart';
 
 class ProfileViewBody extends StatelessWidget {
@@ -172,49 +170,7 @@ class ProfileViewBody extends StatelessWidget {
                       ),
                     ),
                     Gap(16.w),
-                    Expanded(
-                      child: BlocListener<ProfileCubit, ProfileState>(
-                        listener: (context, state) {
-                          if (state is ProfileLogoutSuccess) {
-                            AppNavigation.pop(context, useAppRoute: true);
-                            AppDilagos.showToast(
-                              text: 'Logged out successfully',
-                            );
-                            AppNavigation.pushAndRemoveUntil(
-                              context,
-                              LoginView.routeName,
-                              (route) => false,
-                              useAppRoute: true,
-                            );
-                          }
-                          if (state is ProfileLogoutFailure) {
-                            AppDilagos.showErrorMessage(
-                              context,
-                              errMessage: 'Failed to log out',
-                            );
-                          }
-                          if (state is ProfileLogoutLoading) {
-                            AppDilagos.showLoadingBox(context);
-                          }
-                        },
-                        child: DefaultAppButton(
-                          onPressed: () {
-                            context.read<ProfileCubit>().logout();
-                          },
-                          text: 'Log out',
-                          icon: Padding(
-                            padding: const EdgeInsets.only(left: 8.0),
-                            child: Icon(
-                              Icons.logout,
-                              color: AppColors.light,
-                              size: 32.sp,
-                            ),
-                          ),
-                          borderColor: AppColors.light,
-                          borderWidth: 3.w,
-                        ),
-                      ),
-                    ),
+                    Expanded(child: LogoutButtonBloclistener()),
                   ],
                 ),
                 Gap(30.w),
